cf764072784e611d83a076cb920ea855e2d2368a
wikiclarastack <wikiclarastack@gmail.com>
2026-02-09 21:09:14 -0300

    Create page.tsx

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Eclipse Cursos 🌘
+      </h1>
+    </main>
+  )
+}
